d03258f2b25ab17ccbab185d320fd1af35560ffd
JiinSeok <seokjiin1073@gmail.com>
2024-06-04 17:45:22 +0900

    refactor: router에 동적 경로 연결

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main />);
